6fb2809885c1a0276e538e44ba263432819e8dab
ASNik <lienusowl@yandex.ru>
2021-02-09 00:03:31 +0300

    fix, уверен что эти элементы у меня есть

--- angularTest1/src/app/app.component.ts
+++ angularTest1/src/app/app.component.ts
@@ -11,23 +11,23 @@ export class AppComponent {
   isSideNavVisible = true;
   showHideSideClicked() {
     if (this.isSideNavVisible) {
-      document.getElementById('main').style.marginLeft = "0px";
-      document.getElementById('mySidenav').style.width = "0px";
+      document.getElementById('main')!.style.marginLeft = "0px";
+      document.getElementById('mySidenav')!.style.width = "0px";
       this.isSideNavVisible = false;
     } else {
-      document.getElementById('main').style.marginLeft = "300px";
-      document.getElementById('mySidenav').style.width = "300px";
+      document.getElementById('main')!.style.marginLeft = "300px";
+      document.getElementById('mySidenav')!.style.width = "300px";
       this.isSideNavVisible = true;
     }
   }
 
   buttonClickerDetail() {
-    document.getElementById('myRightScreen').style.transform = "translateX(0%)";
-    document.getElementById('main').style.transform = "translateX(-100%)";
+    document.getElementById('myRightScreen')!.style.transform = "translateX(0%)";
+    document.getElementById('main')!.style.transform = "translateX(-100%)";
   }
 
   closeClicked() {
-    document.getElementById('myRightScreen').style.transform = "translateX(100%)";
-    document.getElementById('main').style.transform = "translateX(0%)";
+    document.getElementById('myRightScreen')!.style.transform = "translateX(100%)";
+    document.getElementById('main')!.style.transform = "translateX(0%)";
   }
 }
